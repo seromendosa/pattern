c1d0c40f0a7cf16f326ecb30e00614870a4f90fa
Y********* <y*********@gmail.com>
2024-09-04 23:02:06 +0200

    "seaborn remover"

--- combined.py
+++ combined.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
